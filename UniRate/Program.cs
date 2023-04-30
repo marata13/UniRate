83af9801d17c78a21d72c7c5ca86f0f648f333c2
aboublini <p19117@unipi.gr>
2023-04-30 12:15:38 +0300

    Added navigation to navbar for favorites page (REGISTERED USERS)

--- UniRate/Program.cs
+++ UniRate/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=RegisteredUsers}/{action=FavoritesRegistered}/{id?}");
+    pattern: "{controller=RegisteredUsers}/{action=HomeRegistered}/{id?}");
 
 app.Run();
